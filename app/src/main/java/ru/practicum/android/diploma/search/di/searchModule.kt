331c2052f25160d72feefc589f26292074c08748
alk <l3pxbsk2kh>
2023-10-27 01:38:51 +0300

    Добавлены DI модули + класс APP

---
+++ app/src/main/java/ru/practicum/android/diploma/search/di/searchModule.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.search.di
+
+import org.koin.dsl.module
+
+val searchModule = module {
+
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/team/di/teamModule.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.team.di
+
+import org.koin.dsl.module
+
+val teamModule = module {
+
+}
