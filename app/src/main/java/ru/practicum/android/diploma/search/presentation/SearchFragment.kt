f29d0f6c743038d089a884c8a9e8d75253616724
smitford <jump145@mail.ru>
2023-10-28 19:37:36 +0400

    PRCTM-52 Создание recyclerView, после теста с МОК

--- app/src/main/java/ru/practicum/android/diploma/search/presentation/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/presentation/SearchFragment.kt
@@ -8,9 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
-import ru.practicum.android.diploma.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
-import ru.practicum.android.diploma.databinding.FragmentTeamBinding
+import ru.practicum.android.diploma.search.domain.models.JobInfo
 
 class SearchFragment : Fragment() {
 
@@ -22,7 +22,7 @@ class SearchFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentSearchBinding.inflate(inflater,container,false)
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -30,6 +30,21 @@ class SearchFragment : Fragment() {
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = binding.rvSearch
+        val adapter = JobAdapter({ _ -> Unit })
+        adapter.jobsList = mutableListOf(
+            JobInfo(
+                id = "1234", area = "Moscow", department = "Eda", employerImgUrl = "",
+                employer = "", name = "Android Developer", salary = "from 1 to 100", type = "rab"
+            ),
+            JobInfo(
+                id = "1235", area = "Kiev", department = "Voda", employerImgUrl = "",
+                employer = "", name = "Android Developer", salary = "from 1 to 100", type = "rab"
+            )
+        )
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = adapter
 
     }
 
