bff35a25f7acccc846a4191373fb0c0976092098
smitford <jump145@mail.ru>
2023-10-28 16:31:55 +0400

    PRCTM-52 Создание recyclerView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,13 +19,20 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        buildConfigField(type = "String", name = "HH_ACCESS_TOKEN", value = "\"${developProperties.hhAccessToken}\"")
+        buildConfigField(
+            type = "String",
+            name = "HH_ACCESS_TOKEN",
+            value = "\"${developProperties.hhAccessToken}\""
+        )
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
@@ -51,17 +58,18 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
-    implementation ("androidx.fragment:fragment:1.5.6")
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.5.3")
-    implementation ("androidx.viewpager2:viewpager2:1.0.0")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-    implementation ("io.coil-kt:coil-compose:2.3.0")
-    implementation ("io.insert-koin:koin-android:3.3.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.google.code.gson:gson:2.10")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+    implementation("androidx.fragment:fragment:1.5.6")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("io.coil-kt:coil-compose:2.3.0")
+    implementation("io.insert-koin:koin-android:3.3.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.google.code.gson:gson:2.10")
 
 //    implementation("androidx.room:room-ktx:2.5.1")
 //    implementation("androidx.room:room-runtime:2.5.1")
---
+++ app/src/main/java/ru/practicum/android/diploma/search/data/models/Department.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.search.data.models
+
+data class Department(
+    val id: String,
+    val name: String
+)
