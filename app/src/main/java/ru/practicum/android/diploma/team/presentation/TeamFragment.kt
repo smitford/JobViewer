19d086f74b71262f8503a7ee0e225c321c125dc0
alk <l3pxbsk2kh>
2023-10-28 10:27:49 +0300

    Верстка экрана TEAM, Добавлен bindind. Добавлено наполнение textview

--- app/src/main/java/ru/practicum/android/diploma/team/presentation/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/team/presentation/TeamFragment.kt
@@ -8,21 +8,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentTeamBinding
 
 class TeamFragment : Fragment() {
 
-    private var _binding : FragmentTeamBinding? = null
-    private val binding get() = _binding
+    private lateinit var binding: FragmentTeamBinding
 
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentTeamBinding.inflate(inflater,container,false)
-        return binding?.root
+    ): View {
+        binding = FragmentTeamBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     @RequiresApi(Build.VERSION_CODES.R)
@@ -30,6 +30,10 @@ class TeamFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.tvDeveloper1.text = getText(R.string.developer_1)
+        binding.tvDeveloper2.text = getText(R.string.developer_2)
+        binding.tvDeveloper3.text = getText(R.string.developer_3)
+        binding.tvDeveloper4.text = getText(R.string.developer_4)
 
     }
 
