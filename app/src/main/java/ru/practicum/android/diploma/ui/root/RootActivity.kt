17cdd54004c55c5934d43b726a5a6961b571ca6a
alk <l3pxbsk2kh>
2023-10-28 10:52:04 +0300

    Добавлено взаимодействие с навигационным контроллером

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -1,7 +1,11 @@
 package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
 
@@ -12,6 +16,33 @@ class RootActivity : AppCompatActivity() {
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavBar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavBar.setupWithNavController(navController)
+
+        // На будущее когда потребуется убирать строку команд в заисимости от экрана
+
+//        navController.addOnDestinationChangedListener { _, destination, _ ->
+//            when (destination.id) {
+//                R.id.playerFragment -> {
+//                    bottomNavBar.visibility = View.GONE
+//                }
+//                R.id.newPlaylistFragment -> {
+//                    bottomNavBar.visibility = View.GONE
+//                }
+//                R.id.showAlbumFragment ->{
+//                    bottomNavBar.visibility = View.GONE
+//                }
+//                else -> {
+//                    bottomNavBar.visibility = View.VISIBLE
+//                }
+//            }
+//        }
+
     }
 
     private fun networkRequestExample(accessToken: String) {
