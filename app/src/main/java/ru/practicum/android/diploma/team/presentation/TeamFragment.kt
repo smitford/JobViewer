e3d7cb1b29821bac9b86f638dabd5d340e99c946
alk <l3pxbsk2kh>
2023-10-28 10:08:48 +0300

    Верстка экрана TEAM

---
+++ app/src/main/java/ru/practicum/android/diploma/team/presentation/TeamFragment.kt
@@ -0,0 +1,42 @@
+package ru.practicum.android.diploma.team.presentation
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatDelegate
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.R
+
+class TeamFragment : Fragment() {
+
+    private var _binding : FragmentTeamBinding? = null
+    private val binding get() = _binding
+
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTeamBinding.inflate(inflater,container,false)
+        return binding?.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // logic
+
+
+    }
+
+}
