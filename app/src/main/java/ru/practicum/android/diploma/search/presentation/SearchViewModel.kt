4680fa17861c1262eb614f21fc74f05a00da402e
smitford <jump145@mail.ru>
2023-11-06 17:20:24 +0400

    Работает поиск и состояния экрана

--- app/src/main/java/ru/practicum/android/diploma/search/presentation/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/presentation/SearchViewModel.kt
@@ -20,28 +20,32 @@ class SearchViewModel(
     private val stateLiveData = MutableLiveData(state)
 
     fun loadJobs() {
+        if (filter.request.isBlank()) return
+        stateLiveData.value = SearchStates.Loading
         viewModelScope.launch {
             loadJobsUseCase.execute(filter = filter).collect { jobsInfo ->
-                changeState(jobsInfo)
+                requestHandler(jobsInfo)
             }
         }
     }
 
-    private fun changeState(jobsInfo: JobsInfo) =
+    fun getState() = stateLiveData
+
+    private fun requestHandler(jobsInfo: JobsInfo) =
         when (jobsInfo.responseCodes) {
             Codes.ERROR -> {
-                state = SearchStates.ServerError
+                stateLiveData.value = SearchStates.ServerError
                 Log.d("server error", jobsInfo.responseCodes.name)
             }
 
             Codes.SUCCESS -> {
-                state =
+                stateLiveData.value =
                     jobsInfo.jobs?.let { SearchStates.Success(it) } ?: SearchStates.InvalidRequest
                 Log.d("success", jobsInfo.responseCodes.name)
             }
 
             Codes.NO_NET_CONNECTION -> {
-                state = SearchStates.ConnectionError
+                stateLiveData.value = SearchStates.ConnectionError
                 Log.d("internet error", jobsInfo.responseCodes.name)
             }
         }
