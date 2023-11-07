f222b8283352e9afec75bf94ff2635537d943c5d
Andrey_W <Uzzbagoyzza@yandex.ru>
2023-11-07 18:18:09 +0300

    Добавил переходы с фрагмента Настройки фильтра

--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/FilterSettingsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/FilterSettingsFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFilterSettingsBinding
 import ru.practicum.android.diploma.filter.presentation.view_model.FilterSettingsViewModel
 
@@ -23,6 +25,21 @@ class FilterSettingsFragment : Fragment() {
     ): View? {
         _binding = FragmentFilterSettingsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ibArrowBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.tvIndustry.setOnClickListener {
+            findNavController().navigate(R.id.action_filterSettingsFragment_to_choosingIndustryFragment2)
+        }
+        binding.tvPlaceOfWork.setOnClickListener {
+            findNavController().navigate(R.id.action_filterSettingsFragment_to_placesOfWorkFragment2)
+        }
     }
 
     override fun onDestroyView() {
