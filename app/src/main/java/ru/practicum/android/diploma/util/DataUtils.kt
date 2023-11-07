45ec35fbcd9d3a1ff1028c308e261d9dfe73fe18
Andrey_W <Uzzbagoyzza@yandex.ru>
2023-11-08 01:07:36 +0300

    Обеспечил хранение и получение информации о фильтре с помощью sharedPreference. Добавил все в di

---
+++ app/src/main/java/ru/practicum/android/diploma/util/DataUtils.kt
@@ -0,0 +1,8 @@
+package ru.practicum.android.diploma.util
+
+class DataUtils {
+    companion object {
+        const val FILTER_SETTINGS = "FILTER_SETTINGS"
+        const val APP_SETTINGS = "APP_SETTINGS"
+    }
+}
