d4d29eab8bf1c92a6452d4a5ce203c9c9471f397
Andrey_W <Uzzbagoyzza@yandex.ru>
2023-11-09 15:41:03 +0300

    Создал ViewModel

--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/PlacesOfWorkFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/PlacesOfWorkFragment.kt
@@ -6,14 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentPlacesOfWorkBinding
+import ru.practicum.android.diploma.filter.presentation.view_model.PlaceOfWorkViewModel
 
 
 class PlacesOfWorkFragment : Fragment() {
 
     private var _binding : FragmentPlacesOfWorkBinding? = null
     private val binding get() = _binding!!
+
+    private val vM : PlaceOfWorkViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
