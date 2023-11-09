c8dcdca41cad82f35c7f983fdd29c04ea2889d05
sa-63 <saa-aka-mr.blonde@yandex.ru>
2023-11-10 00:54:36 +0400

    save args для тестов

--- app/src/main/java/ru/practicum/android/diploma/job/presentation/fragment/JobFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/job/presentation/fragment/JobFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.databinding.FragmentJobBinding
 import ru.practicum.android.diploma.job.presentation.viewmodel.JobFragmentViewModel
@@ -13,6 +14,7 @@ class JobFragment : Fragment() {
 
     private lateinit var binding: FragmentJobBinding
     private val jobFragmentViewModel: JobFragmentViewModel by viewModel()
+    private val args: JobFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +26,6 @@ class JobFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        jobFragmentViewModel.getJob("83456644")
+        jobFragmentViewModel.getJob(args.jobId)
     }
 }
