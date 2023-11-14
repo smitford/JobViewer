b81aed15f2a219b62ec7a98876a65b2afd6b2ea6
alk <l3pxbsk2kh>
2023-11-14 13:55:34 +0300

    Почищено Root activity

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -6,16 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
 
 class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
-
-        // Пример использования access token для HeadHunter API
-        networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
@@ -26,37 +22,24 @@ class RootActivity : AppCompatActivity() {
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
-                R.id.filterSettingsFragment -> {
-                    bottomNavBar.visibility = View.GONE
-                }
-                R.id.placesOfWorkFragment -> {
-                    bottomNavBar.visibility = View.GONE
-                }
-                R.id.countrySelectionFragment ->{
-                    bottomNavBar.visibility = View.GONE
-                }
-                R.id.choosingRegionFragment ->{
-                    bottomNavBar.visibility = View.GONE
-                }
-                R.id.choosingIndustryFragment ->{
-                    bottomNavBar.visibility = View.GONE
-                }
-                R.id.jobFragment ->{
-                    bottomNavBar.visibility = View.GONE
-                }
-                R.id.similarJobFragment ->{
-                    bottomNavBar.visibility = View.GONE
-                }
-                else -> {
-                    bottomNavBar.visibility = View.VISIBLE
-                }
+
+                R.id.filterSettingsFragment -> bottomNavBar.visibility = View.GONE
+
+                R.id.placesOfWorkFragment -> bottomNavBar.visibility = View.GONE
+
+                R.id.countrySelectionFragment -> bottomNavBar.visibility = View.GONE
+
+                R.id.choosingRegionFragment -> bottomNavBar.visibility = View.GONE
+
+                R.id.choosingIndustryFragment -> bottomNavBar.visibility = View.GONE
+
+                R.id.jobFragment -> bottomNavBar.visibility = View.GONE
+
+                R.id.similarJobFragment -> bottomNavBar.visibility = View.GONE
+
+                else -> bottomNavBar.visibility = View.VISIBLE
+
             }
         }
-
     }
-
-    private fun networkRequestExample(accessToken: String) {
-        var t = 0
-    }
-
 }
