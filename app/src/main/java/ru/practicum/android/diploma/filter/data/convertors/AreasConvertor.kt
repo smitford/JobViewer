ba365f07ac74989a234becc578e591ffe382e7f6
Andrey_W <Uzzbagoyzza@yandex.ru>
2023-11-16 03:41:55 +0300

    Исправил функции конвертации для сохранения страны

--- app/src/main/java/ru/practicum/android/diploma/filter/data/convertors/AreasConvertor.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/data/convertors/AreasConvertor.kt
@@ -5,6 +5,9 @@ import ru.practicum.android.diploma.filter.domain.models.Area
 
 object AreasConvertor {
 
+    private var countryId =""
+    private var countryName  = ""
+
 //    fun areasResponseToAreaDto(allAreasResponse: AllAreasResponse): AreaDto {
 //        return AreaDto(
 //            name = allAreasResponse.name,
@@ -14,20 +17,25 @@ object AreasConvertor {
 //        )
 //    }
 
-    fun convertAreasDtoToAreaList(areaDto: AreaDto, parentName: String? = null): List<Area> {
+    fun convertAreasDtoToAreaList(areaDto: AreaDto): List<Area> {
         val areas = mutableListOf<Area>()
-        if (areaDto.parentId != null) {
+
+        if (areaDto.parentId == null){
+            countryName = areaDto.name
+            countryId = areaDto.id
+        }
+        if (areaDto.parentId != null){
             areas.add(
                 Area(
                     name = areaDto.name,
                     id = areaDto.id,
-                    parentName = parentName ?: "No parent id",
-                    parentId = areaDto.parentId
+                    countryName = countryName,
+                    countryId = countryId
                 )
             )
         }
         areaDto.areas.forEach { childArea ->
-            areas.addAll(convertAreasDtoToAreaList(childArea, areaDto.name))
+            areas.addAll(convertAreasDtoToAreaList(childArea))
         }
         return areas
     }
@@ -35,8 +43,9 @@ object AreasConvertor {
     fun convertAreasDtoListToAreaList(areaDtoList: List<AreaDto>): List<Area> {
         val resultList = mutableListOf<Area>()
         areaDtoList.forEach { areaDto ->
-            resultList.addAll(convertAreasDtoToAreaList(areaDto, areaDto.name))
+            resultList.addAll(convertAreasDtoToAreaList(areaDto))
         }
         return resultList
     }
+
 }
--- app/src/main/java/ru/practicum/android/diploma/filter/domain/models/Area.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/domain/models/Area.kt
@@ -3,6 +3,6 @@ package ru.practicum.android.diploma.filter.domain.models
 data class Area(
     val name: String,
     val id: String,
-    val parentName: String,
-    val parentId: String
+    val countryName: String,
+    val countryId: String
 )
