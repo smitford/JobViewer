e9f10d2c0b16856a6498a2f35a49be1e8be07087
sa-63 <saa-aka-mr.blonde@yandex.ru>
2023-11-22 21:09:42 +0400

    fix кнопок в вакансии при загрузке

--- app/src/main/java/ru/practicum/android/diploma/job/presentation/states/JobScreenState.kt
+++ app/src/main/java/ru/practicum/android/diploma/job/presentation/states/JobScreenState.kt
@@ -5,7 +5,6 @@ import ru.practicum.android.diploma.job.domain.models.JobForScreen
 sealed class JobScreenState {
     data object Loading : JobScreenState()
     data object ServerError : JobScreenState()
-    data object ConnectionError : JobScreenState()
     data object InvalidRequest : JobScreenState()
     data class Success(val jobForScreen: JobForScreen) : JobScreenState()
     data class FavouriteIcon(val isFavourite: Boolean) : JobScreenState()
